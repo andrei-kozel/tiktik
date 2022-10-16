92d2aa08fc61fe885dbcb1336b834b0de5bfddfe
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-10-16 13:07:48 +0200

    footer completed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { footerList1, footerList2, footerList3 } from "../utils/constants";
+
+type Props = {
+  items: string[];
+  mt: boolean;
+};
+
+const List = ({ items, mt }: Props) => (
+  <div className={`flex flex-wrap gap-2 ${mt && "mt-5"}`}>
+    {items.map((item) => (
+      <p
+        key={item}
+        className="text-gray-400 text=sm hover:underline cursor-pointer"
+      >
+        {item}
+      </p>
+    ))}
+  </div>
+);
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mt-6 hidden xl:block">
+      <List items={footerList1} mt={false} />
+      <List items={footerList2} mt={true} />
+      <List items={footerList3} mt={true} />
+      <p className="mt-6 text-gray-400 text-sm">
+        {new Date().getFullYear()} TikTik
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
