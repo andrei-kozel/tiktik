d5df144030803d2caebb73d9bcabd00f86b6d3c4
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-10-20 17:29:11 +0200

    implemented NoResult component

--- components/NoResult.tsx
+++ components/NoResult.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { MdOutlineVideocamOff } from "react-icons/md";
+import { BiCommentX } from "react-icons/bi";
 
 interface IPorps {
   text: string;
 }
 
 const NoResult = ({ text }: IPorps) => {
-  return <div>NoResult</div>;
+  return (
+    <div className="flex flex-col justify-center items-center h-full w-full ">
+      <p className="text-8xl">
+        {text.includes("comment") ? <BiCommentX /> : <MdOutlineVideocamOff />}
+      </p>
+      <p className="text-2xl text-center">{text}</p>
+    </div>
+  );
 };
 
 export default NoResult;
